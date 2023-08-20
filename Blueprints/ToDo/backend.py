9be01737a85b8ed3dc5c7a56c823c31d873ac433
A Geddes <onelunatictheory@gmail.com>
2023-08-20 01:06:51 +0100

    Diary works now

--- Blueprints/ToDo/backend.py
+++ Blueprints/ToDo/backend.py
@@ -109,31 +109,6 @@ def add_task(description, tags):
         conn.rollback()
         raise
 
-def get_password_for_user(user_id):
-    conn =get_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT password FROM users WHERE user_id = ?", (user_id, ))
-    result = cursor.fetchone()
-    if result:
-        print(result['password'])
-        return result['password']
-    else:
-        print('user not found')
-        return None
-
-def create_user(user_name, password):
-    conn=get_db()
-    if get_user_id(user_name): return
-    user_id = str(uuid.uuid4())
-    try:
-        conn.execute('BEGIN TRANSACTION')
-        conn.execute('INSERT INTO entities(entity_id, table_index) VALUES (?, ?)', (user_id, g.table_ids.users))
-        conn.execute('INSERT INTO users(user_name, user_id, password) VALUES (?, ?, ?)', (user_name, user_id, password))
-        conn.commit()  
-    except:
-        conn.rollback()
-        raise
-
 def remove_task(task_id):
     conn = get_db()
     sql = ''' DELETE FROM tasks WHERE task_id = ? '''
